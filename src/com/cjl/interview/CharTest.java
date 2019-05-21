d4f65663cb660d745bc569a1bb8c060c8f911f5b
chen.jianliang <chen.jianliang@whaley.cn>
2019-05-21 14:55:13 +0800

    merge

---
+++ src/com/cjl/interview/CharTest.java
@@ -0,0 +1,10 @@
+package com.cjl.www;
+
+public class CharTest {
+
+    public static void main(String[] args) {
+        char ch = '北';
+
+        System.out.println(ch);
+    }
+}
---
+++ src/com/cjl/interview/SingleTon.java
@@ -0,0 +1,35 @@
+package com.cjl.www;
+
+public class SingleTon {
+
+//    private static SingleTon singleTon = new SingleTon();
+//
+//    /**
+//     * private 外部不可创建
+//     */
+//    private SingleTon() {
+//    }
+//
+//    /**
+//     *
+//     * static　
+//     */
+//    public static SingleTon getInstance(){
+//        return singleTon;
+//    }
+
+
+
+    private static SingleTon singleTon;
+
+    private SingleTon(){
+
+    }
+
+    public static SingleTon getInstance(){
+        if (null == singleTon){
+            singleTon = new SingleTon();
+        }
+        return singleTon;
+    }
+}
