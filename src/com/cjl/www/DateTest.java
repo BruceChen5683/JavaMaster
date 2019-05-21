22e5384d65178346590f163b0341398fa5501c2f
chen.jianliang <chen.jianliang@whaley.cn>
2019-05-21 14:49:27 +0800

    匿名内部类

---
+++ src/com/cjl/www/DateTest.java
@@ -0,0 +1,23 @@
+package com.cjl.www;
+
+import java.util.Date;
+
+public class DateTest {
+
+    @SuppressWarnings("deprecation")
+    public String get(Date date){
+        return date.toLocaleString();
+    }
+
+    public static void main(String[] args) {
+        DateTest test  = new DateTest();
+        System.out.println(test.get(new Date(){
+//            @Override
+//            public String toLocaleString() {
+//                return "hello word";
+//            }
+        }));
+
+    }
+
+}
