166ad577409a153b85228ffb6e0993155c32da3f
chen.jianliang <chen.jianliang@whaley.cn>
2019-05-23 14:15:17 +0800

    inserction sort

---
+++ src/com/cjl/algorithm/InsertionSort.java
@@ -0,0 +1,13 @@
+package com.cjl.algorithm;
+
+public class InsertionSort extends MySortExt{
+    @Override
+    public int[] sort(int[] array) {
+        if (array.length <= 1){
+            return array;
+        }
+
+
+        return array;
+    }
+}
