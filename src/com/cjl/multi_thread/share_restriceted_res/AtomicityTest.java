8df39c1741abfbaaec2c9f2a34b7087015690998
chen.jianliang <chen.jianliang@whaley.cn>
2019-05-23 15:24:51 +0800

    AtomicInteger 消除synchronized

--- src/com/cjl/multi_thread/share_restriceted_res/AtomicityTest.java
+++ src/com/cjl/multi_thread/share_restriceted_res/AtomicityTest.java
@@ -2,13 +2,16 @@ package com.cjl.multi_thread.share_restriceted_res;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicityTest implements Runnable{
-    private int i = 0;
-    public int getValue(){return i;}
+//    private int i = 0;
+    private AtomicInteger i = new AtomicInteger(0);
+    public int getValue(){return i.get();}
     private synchronized void evenIncrement(){
-        i++;
-        i++;
+//        i++;
+//        i++;
+        i.addAndGet(2);
     }
     @Override
     public void run() {
