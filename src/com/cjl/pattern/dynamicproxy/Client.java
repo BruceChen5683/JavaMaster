c5b6fc37dc961be872ee0d0330ca8b410a65d22a
chen.jianliang <chen.jianliang@whaley.cn>
2019-12-12 19:16:07 +0800

    dynamic proxy

--- src/com/cjl/pattern/dynamicproxy/Client.java
+++ src/com/cjl/pattern/dynamicproxy/Client.java
@@ -1,6 +1,7 @@
 package com.cjl.pattern.dynamicproxy;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
@@ -12,38 +13,47 @@ public class Client {
         RealSubject realSubject = new RealSubject();
 
 
-        InvocationHandler handler = new DynamicProxySubject();
+        DynamicProxySubject dynamicProxySubject = new DynamicProxySubject();
+        dynamicProxySubject.setSub(realSubject);
 
-        Class<?> classType = handler.getClass();
-
-        Subject subject = (Subject) Proxy.newProxyInstance(
-                classType.getClassLoader(),
-                 realSubject.getClass().getInterfaces(),
-                handler);
-
-
+        Subject subject = ((Subject)dynamicProxySubject.getProxyInstance());
         subject.request();
 
-        System.out.println(subject.getClass());
-
-
-
-        RealSubject2 realSubject2  = new RealSubject2();
-        handler = new DynamicProxySubject(realSubject2);
-        Subject2 subject2 = (Subject2) Proxy.newProxyInstance(
-                realSubject2.getClass().getClassLoader(),
-//                classType.getClassLoader(),
-                realSubject2.getClass().getInterfaces(),
-                handler);
-
-        int t = subject2.set(10);
-        System.out.println("t = " + t);
-
-
-        System.out.println(realSubject.getClass().getClassLoader());
-        System.out.println(realSubject2.getClass().getClassLoader());
-        System.out.println(classType.getClassLoader());
-
+//        System.out.println("Client.main --- "+subject);
+//
+//        subject.request();
+//
+//        System.out.println(subject.getClass());
+//
+//
+//
+//        RealSubject2 realSubject2  = new RealSubject2();
+//        handler = new DynamicProxySubject(realSubject2);
+//        Subject2 subject2 = (Subject2) Proxy.newProxyInstance(
+//                realSubject2.getClass().getClassLoader(),
+////                classType.getClassLoader(),
+//                realSubject2.getClass().getInterfaces(),
+//                handler);
+//
+//        int t = subject2.set(10);
+//        System.out.println("t = " + t);
+//
+//
+//        System.out.println(realSubject.getClass().getClassLoader());
+//        System.out.println(realSubject2.getClass().getClassLoader());
+//        System.out.println(classType.getClassLoader());
+//
+//        RealSubject realSubject1 = new RealSubject();
+//        Object proxy = Proxy.newProxyInstance(realSubject1.getClass().getClassLoader(),
+//                realSubject1.getClass().getInterfaces(),
+//                new InvocationHandler() {
+//                    @Override
+//                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//                        return null;
+//                    }
+//                });
+//
+//        System.out.println("Client.main " + proxy.getClass().getName());
 
 
 
--- src/com/cjl/pattern/dynamicproxy/DynamicProxySubject.java
+++ src/com/cjl/pattern/dynamicproxy/DynamicProxySubject.java
@@ -2,6 +2,7 @@ package com.cjl.pattern.dynamicproxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * Created by chenjianliang on 2017/5/10.
@@ -9,16 +10,17 @@ import java.lang.reflect.Method;
 public class DynamicProxySubject implements InvocationHandler {
     private Object sub;
 
-    public DynamicProxySubject(Object obj){
-        this.sub = obj;
+    public Object getSub() {
+        return sub;
     }
 
     public void setSub(Object sub) {
         this.sub = sub;
     }
 
-    public DynamicProxySubject(){
-
+    public Object getProxyInstance(){
+        return Proxy.newProxyInstance(sub.getClass().getClassLoader(),
+                sub.getClass().getInterfaces(),this);
     }
 
     @Override
@@ -29,7 +31,6 @@ public class DynamicProxySubject implements InvocationHandler {
         System.out.println("proxy.getClass() = " + proxy.getClass());
 
         Object object = method.invoke(sub,args);
-
         System.out.println("after call   "+method);
         return object;
     }
