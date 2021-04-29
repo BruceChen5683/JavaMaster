74f7bb833a704b0bfc10d641c0ec38346ad7bac0
chenjianliang <20024161@suning.com>
2021-04-29 14:43:17 +0800

    exception

--- src/com/cjl/exception/ExceptionTest.java
+++ src/com/cjl/exception/ExceptionTest.java
@@ -19,16 +19,59 @@ public class ExceptionTest {
         throw  new MyException("哈哈");
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
 
-        ExceptionTest exceptionTest = new ExceptionTest();
+//
+//        ExceptionTest exceptionTest = new ExceptionTest();
+//        try {
+//            exceptionTest.set();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }finally {
+//            System.out.println("ddddd");
+//        }
+
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, Throwable throwable) {
+                System.out.println("thread = " + thread.getName() + ", throwable = " + throwable.getMessage());
+            }
+        });
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                int i = 1/0;
+
+            }},"child").start();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("end ...");
+            }
+        },"other child").start();
+
+        int i = 1/0;
+
         try {
-            exceptionTest.set();
-
-        }catch (Exception e){
+            Thread.sleep(8000);
+            System.out.println("end main 8s");
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
-            System.out.println("ddddd");
+            System.out.println("exception "+e.getMessage());
         }
     }
 
